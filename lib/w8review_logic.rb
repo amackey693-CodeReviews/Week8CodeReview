dcd82ab8d661fa999e370420e3156fb94f3797c8
Allison Mackey <a.mackey693@gmail.com>
2020-05-08 09:44:37 -0700

    write & test spec #2 'takes in a second word from the user and splits it into an array'

--- lib/w8review_logic.rb
+++ lib/w8review_logic.rb
@@ -2,9 +2,16 @@
 require('pry') 
 
 class Word 
-
-  def anagram?(word1)
-    word1 = word1.split('')
-    word1 
+  def initialize(word1, word2)
+    @word1 = word1
+    @word2 = word2
   end 
+
+  def anagram?
+    @word1 = @word1.split('')
+    @word1
+  end 
+
+  
+
 end 
--- spec/w8review_spec.rb
+++ spec/w8review_spec.rb
@@ -3,7 +3,11 @@ require('w8review_logic')
 
 describe('#Word') do 
   it("takes in one word from the user and splits it into an array") do 
-    words = Word.new()
-    expect(words.anagram?("read")).to(eq(['r', 'e', 'a', 'd']))
+    words = Word.new("read","dear")
+    expect(words.anagram?).to(eq(['r', 'e', 'a', 'd']))
+  end
+  it("takes in a second word from the user and splits it into a hash") do 
+    words = Word.new("read", "dear")
+    expect(words.word2).to(eq(['d', 'e', 'a', 'r'] ))
   end
 end 
