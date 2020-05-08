1548ab5b53abac49b5315f14e69a68687ab98cde
Allison Mackey <a.mackey693@gmail.com>
2020-05-08 09:45:11 -0700

    test FAILED as expected

--- spec/w8review_spec.rb
+++ spec/w8review_spec.rb
@@ -6,7 +6,7 @@ describe('#Word') do
     words = Word.new("read","dear")
     expect(words.anagram?).to(eq(['r', 'e', 'a', 'd']))
   end
-  it("takes in a second word from the user and splits it into a hash") do 
+  it("takes in a second word from the user and splits it into an array") do 
     words = Word.new("read", "dear")
     expect(words.word2).to(eq(['d', 'e', 'a', 'r'] ))
   end
