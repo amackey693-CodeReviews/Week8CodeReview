8815ee2a3c8972ab2d8dd0259812e4d1f821db0f
Allison Mackey <a.mackey693@gmail.com>
2020-05-08 09:30:30 -0700

    refactor test on spec #1 'takes in one word from the user and splits it into an array'. Test PASSES

--- lib/w8review_logic.rb
+++ lib/w8review_logic.rb
@@ -2,13 +2,9 @@
 require('pry') 
 
 class Word 
-  def initialize(word1, word2)
-    @word1 = word1
-    @word2 = word2
-  end 
-  
-  def anagram?()
-    @word1 = @word1.split('')
-    @word2 = @word2.split('')
+
+  def anagram?(word1)
+    word1 = word1.split('')
+    word1 
   end 
 end 
--- spec/w8review_spec.rb
+++ spec/w8review_spec.rb
@@ -2,8 +2,8 @@ require('rspec')
 require('w8review_logic')
 
 describe('#Word') do 
-  it("takes in two words from the user and splits them into arrays") do 
-    words = Word.new("read", "dear")
-    expect(words.anagram?).to(eq(['r', 'e', 'a', 'd'] ['d', 'e', 'a','r']))
+  it("takes in one word from the user and splits it into an array") do 
+    words = Word.new()
+    expect(words.anagram?("read")).to(eq(['r', 'e', 'a', 'd']))
   end
 end 
