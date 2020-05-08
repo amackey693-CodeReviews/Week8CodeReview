54c8066eb50396cc551ba7feb339a83e92c593d5
Allison Mackey <a.mackey693@gmail.com>
2020-05-08 13:35:04 -0700

    attempt to refactor code to simplify method

--- lib/w8review_logic.rb
+++ lib/w8review_logic.rb
@@ -6,60 +6,32 @@ class Word
     @word1 = word1.split('')
     @word2 = word2.split('')
   end 
-
+  
   def anagram?
-    answer = []
-    vowels = ["e", "i", "o", "u"]
-
-    if (@word1 - vowels).length > 0 
-      puts "yes"
-    end
-    if (@word2 - vowels).length > 0
-      puts "yes"
-    end
-    compare = @word1 - @word2
-    
-    if compare.length == [] #checks to see that word 1 & word2 contain the same letters
-      answer = "Congratulations! You've found a anagram!"
-    elsif compare.length > 0 
+    if  (vowels?(@word1) == true) && (vowels?(@word2) == true)
+      compare = @word1 - @word2
+      if compare.length == [] #checks to see that word 1 & word2 contain the same letters
+        answer = "Congratulations! You've found a anagram!"
+      elsif compare.length > 0 
+        answer = "One of these things are not like the other, please try again!"
+      end 
+    elsif 
       answer = "One of these things are not like the other, please try again!"
-    end 
-  
-    #  @word1.each_with_index do |ltr| ## checks to see if word #1 conatins vowels
-    #     if word?.include?(ltr)
-    #       answer.push(word?.fetch(ltr))
-    #     end
-    #   end
-    #   @word2.each_with_index do |ltr| ## checks to see if word #2 conatins vowels
-    #     if word?.include?(ltr)
-    #       answer.push(word?.fetch(ltr))
-    #     else 
-    #       answer.push(false)
-    #     end
-    #   end
-    #   if answer.include?(true) ## moves to next step in comparing to make sure letters are the same
-        
-    #   end 
-    # else
-    #   answer.push(false)
-    # end 
-    
-  end
-  
-  # def word?
-  #   vowels = {"a" => true, "e" => true, "i" => true, "o" => true, "u" => true}
-  # end 
-end 
+    end
+    answer
+  end 
 
- 
-    # if @word1.length == @word2.length
-    #   @word1.each_with_index do |ltr| 
-    #     if @word2.all? {|i| i.include?(ltr)} 
-    #       answer.push(true)
-    #     else
-    #       answer.push(false)
-    #     end
-    #   end
-    # else 
-    #   answer.push(false)
-    # end 
+  def vowels?(array)
+    array.each_with_index do |ltr| 
+      if find_vowels.include?(ltr)
+        return true
+      else
+        return false
+      end
+    end
+  end
+
+  def find_vowels
+   vowels = {"a" => true, "e" => true, "i" => true, "o" => true, "u" => true}
+  end
+end 
